7b29ffd25349831ce92a188922d8b8a8c9f20ff2
guroznur <63719697+guroznur@users.noreply.github.com>
2021-03-18 22:04:44 +0300

    updated

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -12,9 +14,13 @@ export class ProductComponent implements OnInit {
   
   products:Product [] = [];
   dataLoaded = false;
+  filterText="";
  
   
-  constructor(private productService:ProductService,private activatedRoute:ActivatedRoute) {}
+  constructor(private productService:ProductService,
+    private activatedRoute:ActivatedRoute, 
+    private toastrService:ToastrService,
+    private cartService:CartService) {}
 
   ngOnInit(): void {
   
@@ -38,10 +44,21 @@ export class ProductComponent implements OnInit {
    
   }
   getProductsByCategory(categoryId:number){
-    this.productService.getProductsByCategoryId(categoryId).subscribe(response=>{
+    this.productService.getProductsByCategory(categoryId).subscribe(response=>{
       this.products = response.data
       this.dataLoaded = true;
     })
    
+  }
+
+  addToCart(product:Product){
+
+    if(product.productId===1){
+      this.toastrService.error("Hata","Bu ürün sepete eklenemez")
+    }else{
+        this.toastrService.success("Sepete eklendi",product.productName)
+      }
+       
+    this.cartService.addToCart(product);
   }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,29 +1,25 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { ListResponseModel } from '../models/listResponseModel';
 import { Product } from '../models/product';
-import { ListResponseModel } from '../models/ListResponseModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  apiUrl = "https://localhost:44339/api/";
+  apiUrl = 'https://localhost:44314/api/';
 
-  constructor(private httpclient:HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
-  getProducts():Observable<ListResponseModel<Product>>{
-    let newPath = this. apiUrl + "products/getall"
-   return this.httpclient.get<ListResponseModel<Product>>(newPath);
-    
-    }
-
-  getProductsByCategoryId(categoryId:number):Observable<ListResponseModel<Product>>{
-    let newPath = this. apiUrl + "products/getbycategory?categoryId="+categoryId
-      return this.httpclient.get<ListResponseModel<Product>>(newPath);
-       
-       }
-
+  getProducts():Observable<ListResponseModel<Product>> {
+    let newPath = this.apiUrl + "products/getall"
+    return this.httpClient.get<ListResponseModel<Product>>(newPath);
   }
 
+  getProductsByCategory(categoryId:number):Observable<ListResponseModel<Product>> {
+    let newPath = this.apiUrl + "products/getbycategory?categoryId="+categoryId
+    return this.httpClient.get<ListResponseModel<Product>>(newPath);
+  }
+}
